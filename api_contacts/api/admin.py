8042699400b284bac535a1d4dd90db345acb2aef
DariaKalinichenko <kddm2015@gmail.com>
2020-12-23 20:47:59 +0300

    Created models

--- api_contacts/api/admin.py
+++ api_contacts/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Employee, DateContact
+
+admin.site.register(Client)
+
+admin.site.register(Employee)
+
+admin.site.register(DateContact)
--- api_contacts/api/models.py
+++ api_contacts/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=200, verbose_name='ФИО клиента')
+    age = models.IntegerField(verbose_name='Возраст')
+    address = models.CharField(max_length=200, verbose_name='Адрес проживания')
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=200, verbose_name='ФИО сотрудника')
+    position = models.CharField(max_length=100, verbose_name='Должность сотрудника')
+    client = models.ManyToManyField(
+        Client,
+        through='DateContact')
+
+    def __str__(self):
+        return self.name
+
+
+class DateContact(models.Model):
+    date = models.DateTimeField(verbose_name='Дата контактирования')
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE,
+                                 related_name='employee')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE,
+                               related_name='client')
